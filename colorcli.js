0624493be83025053bf8267a921c0a8e3cb1601d
Ralph <ralph@wireless-nat-inside.uiowa.edu>
2019-02-15 13:54:50 -0600

    Read default color from config.json

--- colorcli.js
+++ colorcli.js
@@ -9,6 +9,14 @@ let config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 const color = config.color || process.argv[3] || "blue";
 const text = process.argv[2] || "";
 
-console.log(chalk.keyword(color)(text));
+if (text == "setcolor") {
+    config.color = process.argv[3];
+    fs.writeFile('./config.json', JSON.stringify(config), err => {
+        if (err) throw err;
+        console.log(`Default color has been set to ${process.argv[3]}`);
+    });
+} else {
 
+    console.log(chalk.keyword(color)(text));
+}
 
