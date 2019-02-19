afe7ed9a6fd5b90d34620b584e00f335850dafea
Ralph <ralph@wireless-nat-inside.uiowa.edu>
2019-02-19 11:16:54 -0600

    Implement hex support + refactor application of color

--- colorcli.js
+++ colorcli.js
@@ -6,12 +6,22 @@ const cli = require('cli');
 
 cli.parse(
     {
-        color: ['c', 'The keyword for the color to be applied to the provided text', 'string', 'orange']
+        color: ['c', 'The keyword for the color to be applied to the provided text', 'string', 'orange'],
+        hex: ['h', 'The hex code of the color to be applied to the provided text. Do not include #', 'string', false],
     }
 );
 
+let applyColor = () => {};
+
+if (cli.options.hex) {
+    applyColor = chalk.hex('#' + cli.options.hex);
+} else {
+    applyColor = chalk.keyword(cli.options.color);
+}
+
 let text = cli.args.join(' ');
-console.log(chalk.keyword(cli.options.color)(text));
+console.log(applyColor(text));
 
 //TODO: Add support for setting a default color stored in a json file
-//TODO: Add support for setting color using rgb, hex, etc
+//TODO: Add RGB support
+//FIX: Try to make it so that hex color works regardless if # is present or not
